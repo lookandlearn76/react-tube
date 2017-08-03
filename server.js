298ccf26b48378908677b16df43228f386a72752
Mark <lookandlearn76@yahoo.co.uk>
2017-08-03 12:23:15 +0100

    fixing heroku deplay

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+//create app
+var app = express();
+
+/*app.use(function (req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});*/
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.listen(PORT, function(){
+  console.log('express server is up and running on port ' + PORT);
+});
