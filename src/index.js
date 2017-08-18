613e2283fc22e26675532744241bec3a129aa6ee
Mark <lookandlearn76@yahoo.co.uk>
2017-08-18 22:26:26 +0100

    phew finally back to working

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import VideoList from './components/videoList';
 import VideoInfo from './components/videoInfo';
 
 const API_KEY = 'AIzaSyDicbu-cuTqhkus8eATLtZPH2zAnJuI5BQ';
-
+//.... texting the data with an ajax request......
+// YTSearch({key: API_KEY, term: 'einaudi'}, //function(data){
+//   console.log(data);
+// });
 
 class App extends Component {
   constructor(props) {
@@ -18,24 +21,25 @@ class App extends Component {
       chosenVideo: null
     };
 
-    this.videoSearch('surfboards');
+    this.videoSearch('Einaudi');
   }
 
   videoSearch(term) {
-    YTSearch({key: API_KEY, term: term }, (videos) => {
+    YTSearch({key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
         chosenVideo: videos[0]
       });
     });
-
   }
 
   render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 400);
+    const videoSearch = _.debounce((term) => {this.videoSearch(term); }, 400);
 
     return (
       <div>
+        <h1 className="title">REACT-TUBE</h1>
+        <h2 className="title">The ReactJS video-clip searcher</h2>
         <SearchBar onSearchTermChange={videoSearch} />
         <VideoInfo video={this.state.chosenVideo} />
         <VideoList
